6a2f33ebe62992d1c46516ef5094246e0e91778f
kuu910 <kuu2015910@gmail.com>
2026-04-28 12:07:27 +0900

    Update StageMovement.cs

--- script/StageMovement.cs
+++ script/StageMovement.cs
@@ -7,16 +7,16 @@ public class StageMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float sin = Mathf.Sin( Time.time );
+        float sin = Mathf.Sin(Time.time);
         float x = -3;
         float y = 1;
         float z = 0;
-        this.transform.position = new Vector3(sin*x, y,  z );
+        this.transform.position = new Vector3(sin+x, y, z);
     }
 }
